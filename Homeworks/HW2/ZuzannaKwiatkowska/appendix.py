f13d9fdaa5f389173f2e6b354567618aa41ac171
zkwiatkowska <z.kwiatkowska7@gmail.com>
2022-03-23 15:53:37 +0100

    Update in figures and appendix which adds captions to figures

--- Homeworks/HW2/ZuzannaKwiatkowska/appendix.py
+++ Homeworks/HW2/ZuzannaKwiatkowska/appendix.py
@@ -33,30 +33,34 @@ if __name__ == '__main__':
     print(results.loc[interesting_predictions])
 
     # generate SHAP explanation for 3 predictions
+    titles = [
+        "Example of malignant tumor predicted correctly by XGBoost [patient 29].",
+        "Example of malignant tumor predicted as benign by XGBoost [patient 41].",
+        "Example of benign tumor predicted correctly by XGBoost [patient 285]."
+    ]
     xgboost_explainer = dx.Explainer(xgboost_model, data_test, target_test)
-    for prediction_no in tqdm(interesting_predictions, desc="3 predictions viz gen"):
+    for i, prediction_no in enumerate(interesting_predictions):
         explanation = xgboost_explainer.predict_parts(data_test.loc[[prediction_no]], type="shap")
-        fig = explanation.plot(show=False)
+        fig = explanation.plot(show=False, title=titles[i])
         fig.write_image(f"resources/shap_prediction{prediction_no}.png")
 
-    # generate SHAP explanation with different "highest importance" features (find them first)
-    explanation = xgboost_explainer.predict_parts(data_test.iloc[[0]], type="shap")
-    agg = explanation.result.groupby(by="variable")["contribution"].mean().sort_values(key=abs, ascending=False)
-    feature = agg.index[0].split(" = ")[0]
+    # generate SHAP explanation with different "highest importance" features (auto choice)
+    interesting_predictions = [47, 421]
+    print(results.loc[interesting_predictions])
 
-    for ix in tqdm(data_test.index[1:], desc="HI finder"):
-        c_explanation = xgboost_explainer.predict_parts(data_test.loc[[ix]], type="shap")
-        agg = c_explanation.result.groupby(by="variable")["contribution"].mean().sort_values(key=abs, ascending=False)
-        candidate = agg.index[0].split(" = ")[0]
-        if candidate != feature:
-            explanation.plot(show=False).write_image(f"resources/various_importance{data_test.index[0]}.png")
-            c_explanation.plot(show=False).write_image(f"resources/various_importance{ix}.png")
-            break
+    titles = [
+        "Example of malignant tumor predicted correctly by XGBoost [patient 47].",
+        "Example of correct benign tumor prediction by XGBoost [patient 421]."
+    ]
+    for i, prediction_no in enumerate(interesting_predictions):
+        explanation = xgboost_explainer.predict_parts(data_test.loc[[prediction_no]], type="shap")
+        fig = explanation.plot(show=False, title=titles[i])
+        fig.write_image(f"resources/various_importance{prediction_no}.png")
 
     # generate SHAP explanation where same feature has +/- (omitted due to worst area 29 and 41)
     # train MLP and calculate predictions
     mlp_model = MLPClassifier(max_iter=1000).fit(data_train, target_train)
     mlp_explainer = dx.Explainer(mlp_model, data_test, target_test)
     explanation = mlp_explainer.predict_parts(data_test.loc[[41]], type="shap")
-    fig = explanation.plot(show=False)
+    fig = explanation.plot(show=False, title="Example of malignant tumor predicted correctly by MLP [patient 41].")
     fig.write_image(f"resources/shap_mlp_prediction41.png")
